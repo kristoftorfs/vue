daebe677f3ad82ece8737467eb2e9f3bb1302d01
Kristof Torfs <kristof@torfs.org>
2021-05-04 19:06:27 +0200

    Finished chapter 11

--- webpack.config.js
+++ webpack.config.js
@@ -64,6 +64,11 @@ Encore
     // enables Sass/SCSS support
     .enableSassLoader()
     .enableVueLoader()
+    .configureCssLoader((config) => {
+        if (!Encore.isProduction() && config.modules) {
+            config.modules.localIdentName = '[name]_[local]_[hash:base64:5]';
+        }
+    })
 
     // uncomment if you use TypeScript
     //.enableTypeScriptLoader()
